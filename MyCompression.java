97087b2b0ff313ff57648d0d9eab8a90104398fe
Eitan <vilker@Eitans-MacBook-Air.local>
2018-04-27 18:03:04 -0400

    drtk

--- MyCompression.java
+++ MyCompression.java
@@ -5,7 +5,7 @@ import java.util.TreeMap;
 
 public class MyCompression {
 	
-	public TreeMap<Character, Integer> getFrequencies(BufferedReader input) {		
+	public static TreeMap<Character, Integer> getFrequencies(BufferedReader input) {		
 		TreeMap<Character, Integer> charFreq = new TreeMap<Character, Integer>();
 		boolean iAmDone = false;
 		while (!iAmDone) {
@@ -38,5 +38,7 @@ public class MyCompression {
 	
 	public static void main(String[] args) {
 		BufferedReader input = new BufferedReader(new FileReader(path + "TestCase.txt"));
+		TreeMap<Character, Integer> charFreq = getFrequencies(input);
+		
 	}
 }
--- TreeComparator.java
+++ TreeComparator.java
@@ -1,13 +1,13 @@
 import java.util.TreeMap;
 import java.io.BufferedReader;
-import MyCompression.java.*;
 
 public class TreeComparator {
 
 	public TreeComparator(TreeMap<Character, Integer> charFreq) {
 		for (Character key: charFreq.keySet()) {
 			ValuedCharacter c = new ValuedCharacter(key, charFreq.get(key));
+			BST<Character, Integer> tree = new BST<Character, Integer>();
 		}
-		BST<String, Integer> tree = new BST<String, Integer>();
+		
 	}
 }
