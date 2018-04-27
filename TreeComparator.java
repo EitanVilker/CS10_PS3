10402c3862fd46d6bdc7f01cd768179271f104aa
Eitan <vilker@Eitans-MacBook-Air.local>
2018-04-27 18:23:09 -0400

    qvwc

--- TreeComparator.java
+++ TreeComparator.java
@@ -1,12 +1,16 @@
-import java.util.TreeMap;
-import java.io.BufferedReader;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class TreeComparator {
+public class TreeComparator implements Comparator {
 
 	public TreeComparator(TreeMap<Character, Integer> charFreq) {
+		PriorityQueue<BST<Character, Integer>> theQueue = new PriorityQueue<BST<Character, Integer>>();
 		for (Character key: charFreq.keySet()) {
 			ValuedCharacter c = new ValuedCharacter(key, charFreq.get(key));
 			BST<Character, Integer> tree = new BST<Character, Integer>();
+			tree.insert(key, charFreq.get(key));
+			theQueue.add(tree);
 		}
 		
 	}
