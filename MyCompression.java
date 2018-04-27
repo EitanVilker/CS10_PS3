654af4d29c79be5fbbdf1390b4e9134a31410953
ALowVerus <aidan.low98@gmail.com>
2018-04-27 18:19:59 -0400

    outlined

--- MyCompression.java
+++ MyCompression.java
@@ -37,8 +37,25 @@ public class MyCompression {
 	}
 	
 	public static void main(String[] args) {
+		// Read in the file
 		BufferedReader input = new BufferedReader(new FileReader(path + "TestCase.txt"));
+		// Generate frequency table
 		TreeMap<Character, Integer> charFreq = getFrequencies(input);
+		// Generate frequency leaves from frequency map
+		
+		// Add frequency leaves to priority queue
+		
+		// Generate a tree using frequency leaves
+				
+		// Generate a TreeMap dictionary with characters as keys and 1/0 strings as outputs
+		
+		// Generate compressed file, cross-referencing input file with dictionary
+		
+		// Save compressed file to FilenameCompressed.txt
+		
+		// Generate decompressed file, cross-referencing input file with dictionary
+		
+		// Save decompressed file to FilenameDecompressed.txt'
 		
 	}
 }
