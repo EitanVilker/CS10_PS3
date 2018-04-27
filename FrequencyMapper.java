fe52db21338da8f758da33bf71742dde9da39e22
ALowVerus <aidan.low98@gmail.com>
2018-04-27 16:46:29 -0400

    new classes

--- FrequencyMapper.java
+++ FrequencyMapper.java
@@ -1,8 +1,14 @@
+import java.io.BufferedReader;
+import java.util.ArrayList;
+
 /**
  * Count the frequency of all items in a document, return a list of character frequencies
  * @author alowverus
  *
  */
 public class FrequencyMapper {
-
+	public ArrayList<Integer> getFrequencies(BufferedReader input) {
+		ArrayList<Integer> charFreq = new ArrayList<Integer>();
+		return charFreq;
+	}
 }
