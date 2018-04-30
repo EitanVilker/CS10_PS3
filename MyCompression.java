0ada03463907396d8a9ac6bc2e1c1d9090550fb5
Eitan <vilker@Eitans-MacBook-Air.local>
2018-04-30 19:04:31 -0400

    Fixed bugs

--- MyCompression.java
+++ MyCompression.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -6,6 +7,8 @@ import java.util.PriorityQueue;
 import java.util.TreeMap;
 
 public class MyCompression {
+	
+	//public static final String filepath = "inputs/WarAndPeace.txt";
 	
 	public static TreeMap<Character, Integer> getFrequencies(BufferedReader input) {		
 		TreeMap<Character, Integer> charFreq = new TreeMap<Character, Integer>();
@@ -38,16 +41,15 @@ public class MyCompression {
 		return decompressed;
 	}
 	
-	public static void main(String[] args) {
-		// Read in the file
-		BufferedReader input = new BufferedReader(new FileReader(path + "TestCase.txt"));
+	public static void main(String[] args) { 		// Read in the file
+		try {
+		BufferedReader input = new BufferedReader(new FileReader("Inputs/USConstitution.txt"));
 		// Generate frequency table
-		TreeMap<Character, Integer> charFreq = getFrequencies(input);
 //		// Generate frequency leaves from frequency map		
 //		// Add frequency leaves to priority queue
 //		// Generate a tree using frequency leaves	
-//		// Generate a TreeMap dictionary with characters as keys and 1/0 strings as outputs
-		TreeMap<Character, String> binaryMap = TreeComparator.generateBinaryMapFromFrequencies(charFreq);
+		TreeMap<Character, Integer> charFreq = getFrequencies(input);
+//		// Generate a TreeMap dictionary with characters as keys and bit strings as outputs
 		// Generate compressed file, cross-referencing input file with dictionary
 		
 		// Save compressed file to FilenameCompressed.txt
@@ -55,6 +57,26 @@ public class MyCompression {
 		// Generate decompressed file, cross-referencing input file with dictionary
 		
 		// Save decompressed file to FilenameDecompressed.txt'
+		TreeMap<Character, String> binaryMap = TreeComparator.generateBinaryMapFromFrequencies(charFreq);
+		BufferedBitReader bitInput = new BufferedBitReader("Inputs/USConstitution.txt");
+		BufferedBitWriter bitOutput = new BufferedBitWriter("Outputs/USConstitutionCompressed.txt");
+		while (bitInput.hasNext()) {
+			  boolean bit = bitInput.readBit();
+			  // Find bit sequence, identify corresponding code for character, and write to file
+			  bitOutput.writeBit(bit);
+			}
+		input.close();
+		
+		}
+		
+		catch(FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		} 
+		
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 }
---
+++ TestStrings.java
@@ -0,0 +1,9 @@
+
+public class TestStrings {
+	public static String KungFuFighting() {
+		String s = "";
+		s += "Everybody was kung-fu fighting";
+		s += "Those kicks were fast as lightning";
+		return s;
+	}
+}
