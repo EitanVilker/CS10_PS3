cae85d255571ae6e3b55eb783d0d1aa0778ee580
Eitan <vilker@Eitans-MacBook-Air.local>
2018-04-30 20:44:23 -0400

    before removing valuedcharacter

--- MyCompression.java
+++ MyCompression.java
@@ -10,22 +10,17 @@ public class MyCompression {
 	
 	//public static final String filepath = "inputs/WarAndPeace.txt";
 	
-	public static TreeMap<Character, Integer> getFrequencies(BufferedReader input) {		
+	public static TreeMap<Character, Integer> getFrequencies(String input) {		
 		TreeMap<Character, Integer> charFreq = new TreeMap<Character, Integer>();
-		boolean iAmDone = false;
-		while (!iAmDone) {
-			try {
-				Character nextChar = (char)input.read();
-				if (charFreq.get(nextChar) == null) {
-					charFreq.put(nextChar, 1);
-				}
-				else {
-					int thisFreq = charFreq.get(nextChar);
-					charFreq.put(nextChar, thisFreq + 1);
-				}
+		int length = input.length();
+		for (int i = 0; i < length; i ++) {
+			Character nextChar = input.charAt(i);
+			if (charFreq.get(nextChar) == null) {
+				charFreq.put(nextChar, 1);
 			}
-			catch (IOException e) {
-				iAmDone = true;
+			else {
+				int thisFreq = charFreq.get(nextChar);
+				charFreq.put(nextChar, thisFreq + 1);
 			}
 		}
 		return charFreq;
@@ -43,29 +38,40 @@ public class MyCompression {
 	
 	public static void main(String[] args) { 		// Read in the file
 		try {
-		BufferedReader input = new BufferedReader(new FileReader("Inputs/USConstitution.txt"));
-		// Generate frequency table
-//		// Generate frequency leaves from frequency map		
-//		// Add frequency leaves to priority queue
-//		// Generate a tree using frequency leaves	
-		TreeMap<Character, Integer> charFreq = getFrequencies(input);
-//		// Generate a TreeMap dictionary with characters as keys and bit strings as outputs
-		// Generate compressed file, cross-referencing input file with dictionary
-		
-		// Save compressed file to FilenameCompressed.txt
-		
-		// Generate decompressed file, cross-referencing input file with dictionary
-		
-		// Save decompressed file to FilenameDecompressed.txt'
-		TreeMap<Character, String> binaryMap = TreeComparator.generateBinaryMapFromFrequencies(charFreq);
-		BufferedBitReader bitInput = new BufferedBitReader("Inputs/USConstitution.txt");
-		BufferedBitWriter bitOutput = new BufferedBitWriter("Outputs/USConstitutionCompressed.txt");
-		while (bitInput.hasNext()) {
-			  boolean bit = bitInput.readBit();
-			  // Find bit sequence, identify corresponding code for character, and write to file
-			  bitOutput.writeBit(bit);
+			BufferedReader input = new BufferedReader(new FileReader("Inputs/TestCase.txt"));
+			String s = "";
+			boolean finished = false;
+			while (!finished) {
+				int next = input.read();
+				if (next == -1) {
+					finished = true;
+				}
+				else {
+					s += (char)(next);
+				}
 			}
-		input.close();
+			// Generate frequency table
+			TreeMap<Character, Integer> charFreq = getFrequencies(s);
+			// Generate frequency leaves from frequency map		
+			// Add frequency leaves to priority queue
+			// Generate a tree using frequency leaves	
+			// Generate a TreeMap dictionary with characters as keys and bit strings as outputs
+			TreeMap<Character, String> binaryMap = TreeComparator.generateBinaryMapFromFrequencies(charFreq);
+			// Generate compressed file, cross-referencing input file with dictionary
+			
+			// Save compressed file to FilenameCompressed.txt
+			
+			// Generate decompressed file, cross-referencing input file with dictionary
+			
+			// Save decompressed file to FilenameDecompressed.txt'
+			BufferedBitReader bitInput = new BufferedBitReader("Inputs/USConstitution.txt");
+			BufferedBitWriter bitOutput = new BufferedBitWriter("Outputs/USConstitutionCompressed.txt");
+			while (bitInput.hasNext()) {
+				  boolean bit = bitInput.readBit();
+				  // Find bit sequence, identify corresponding code for character, and write to file
+				  bitOutput.writeBit(bit);
+				}
+			input.close();
 		
 		}
 		
@@ -77,6 +83,8 @@ public class MyCompression {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		finally {
+			
+		}
 	}
 }
